db3d002b323046c6c2e4c7c48325c584d062ec52
Dawid Przydryga <shivva90@gmail.com>
2020-03-03 19:19:56 +0100

    test math.random function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-let computerMove = `kamień`;
+let randomFraction = Math.random();
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Wylosowany ułamek to: ' + randomFraction);
 
-let playerMove = 'papier' ;
+let calculation = randomFraction * (19 - 11) + 11;
 
-printMessage('Zagrałem ' + playerMove + '! Wygrywam');
+printMessage('Ułamek pomnożony przez 8 i powiększony o 11: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
+
+
